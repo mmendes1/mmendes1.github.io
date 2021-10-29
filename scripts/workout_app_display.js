83066084981808a17f83800f0427c67abe679051
Mike Mendes <mmendes1@worcester.edu>
2021-10-29 10:39:02 -0400

    potential fix for file display (js)

--- scripts/workout_app_display.js
+++ scripts/workout_app_display.js
@@ -1,4 +1,4 @@
-var file = document.getElementsById("myFile").files[0];
+var file = document.getElementById("myFile").files[0];
     var reader = new FileReader();
     reader.onload = function(e) {
         var textArea = document.getElementById("myTextArea");
